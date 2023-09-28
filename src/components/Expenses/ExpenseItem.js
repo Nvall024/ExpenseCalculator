0697844614d50433e21e5acfde63f1f8c66d3b0f
Nathaly Valle <nathalyvalle@Nathalys-Air.Home>
2023-09-28 18:35:37 -0500

    started on creating the form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css"
 
 
 function ExpenseItem(props){
+
+  const [title, setTitle] = useState(props.title);
+
   return(
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
